99f5f8a8265afa77295b233cab1e197365b57005
teddyvicker <vicpoliveira@gmail.com>
2023-10-28 16:19:32 -0300

    primeira class

--- p_csharp/Program.cs
+++ p_csharp/Program.cs
@@ -1,4 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// int a = 6;
 
-Console.WriteLine("Hello, World!");
+//int b = 8;
+
+//if (a > b)
+//{
+//    Console.WriteLine("a > b");
+//}
+//else if (a < b)
+//{
+//    Console.WriteLine("b > a");
+//}
+
+//using System.Linq.Expressions;
+
+//Console.WriteLine("Informe um nome: ");
+//string input = Console.ReadLine();
+//Console.WriteLine($"O nome é {input}");
+
+//Console.WriteLine("informe um valor: ");
+
+//int a = int.Parse(Console.ReadLine());
+//int b = 14;
+//if (a > b)
+//{
+//    Console.WriteLine("A é maior que b");
+//}
+//else
+//{
+//    Console.WriteLine("Não é Maior");
+//}
+
+using p_csharp;
+Calculadora.Run();
---
+++ p_csharp/calculadora.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_csharp
+{
+    internal class Calculadora
+    {
+        public static void Run()
+            {
+                Console.WriteLine("CALCULADORA ");
+                Console.WriteLine(" 1 - Soma ");
+                Console.WriteLine(" 2 - Produto ");
+                Console.WriteLine(" 3 - Subtração ");
+                Console.WriteLine(" 4 - Divisão ");
+
+                string op = Console.ReadLine().ToLower();
+
+                Console.WriteLine("Informe o Valor de A");
+                int ValorA = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Informe o Valor de B");
+                int ValorB = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case "1":
+                        Console.WriteLine($"A Soma de {ValorA} e {ValorB} é {ValorA + ValorB}");
+                        break;
+                case "2":
+                    Console.WriteLine($"A Produto de {ValorA} e {ValorB} é {ValorA % ValorB}");
+                    break;
+                case "3":
+                    Console.WriteLine($"A Subtração de {ValorA} e {ValorB} é {ValorA - ValorB}");
+                    break;
+                case "4":
+                    Console.WriteLine($"A Divisao de {ValorA} e {ValorB} é {ValorA / ValorB}");
+                    break;
+                default:
+                        Console.WriteLine("Nao Entendi");
+                        break;
+                }
+
+            }
+        }
+    }
